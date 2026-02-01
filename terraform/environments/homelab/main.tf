1430cdf093ec0c36be1575990910a20a4fce676c
Luiz Glomyer <glomyerjunior@hotmail.com>
2026-02-01 19:10:14 -0400

    feat: terraform definitions for multiple VMs

--- terraform/environments/homelab/main.tf
+++ terraform/environments/homelab/main.tf
@@ -1,14 +1,44 @@
+variable "VMs" {
+  description = "VM definitions for the homelab"
+  type = map(object({
+    vm_id       = number
+    hostname    = string
+    ipv4        = string
+    template_id = number
+  }))
+
+  default = {
+    debian12-01 = {
+      vm_id       = 200
+      hostname    = "debian12-01"
+      ipv4        = "192.168.0.200/24"
+      template_id = 9000
+    }
+
+    debian12-02 = {
+      vm_id       = 201
+      hostname    = "debian12-02"
+      ipv4        = "192.168.0.201/24"
+      template_id = 9001
+    }
+  }
+}
+
+
 resource "proxmox_virtual_environment_file" "cloudinit" {
+  for_each = var.VMs
+
   content_type = "snippets"
   datastore_id = "local"
   node_name    = var.pve_node_name
 
   source_raw {
-    file_name = "debian-cloudinit.yaml"
+    file_name = "${each.key}-cloudinit.yaml"
+
     data = templatefile(
       "${path.module}/../../cloud-init/debian.yaml.tpl",
       {
-        hostname = var.vm_hostname
+        hostname = each.value.hostname
         user     = var.vm_username
         ssh_key  = file(var.ssh_public_key_path)
       }
@@ -17,10 +47,12 @@ resource "proxmox_virtual_environment_file" "cloudinit" {
 }
 
 
-resource "proxmox_virtual_environment_vm" "debian" {
+resource "proxmox_virtual_environment_vm" "debian12" {
+  for_each = var.VMs
+
   node_name = var.pve_node_name
-  name      = var.vm_hostname
-  vm_id     = 200
+  name      = each.value.hostname
+  vm_id     = each.value.vm_id
 
   started                              = true
   machine                              = "q35"
@@ -29,7 +61,7 @@ resource "proxmox_virtual_environment_vm" "debian" {
   delete_unreferenced_disks_on_destroy = true
 
   clone {
-    vm_id = 9000
+    vm_id = each.value.template_id
     full  = true
   }
 
@@ -56,11 +88,11 @@ resource "proxmox_virtual_environment_vm" "debian" {
   }
 
   initialization {
-    user_data_file_id = proxmox_virtual_environment_file.cloudinit.id
+    user_data_file_id = proxmox_virtual_environment_file.cloudinit[each.key].id
 
     ip_config {
       ipv4 {
-        address = "192.168.0.200/24"
+        address = each.value.ipv4
         gateway = "192.168.0.1"
       }
     }
